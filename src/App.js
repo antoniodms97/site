eb0abd1f020e3fd74ecfac858439333b52faf381
Antonio Sasso <antoniodsasso@gmail.com>
2022-08-23 15:11:44 -0700

    Campsite Slice

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CampsitesList from './features/campsites/CampsitesList.js';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+
 
 function App() {
   return (
     <div className="App">
-      Hello nucamp!
+      <Header />
+      <CampsitesList />
+      <Footer />
     </div>
   );
 }
