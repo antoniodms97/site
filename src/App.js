9b16eb7bf21bd3547d3e5dafe4c28fc4347dca1c
Antonio Sasso <antoniodsasso@gmail.com>
2022-08-25 16:45:42 -0700

    CampsiteDetailPage with useParams

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
 
 
@@ -10,7 +14,12 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesDirectoryPage />
+      <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='contact' element={<ContactPage />} />
+          <Route path='directory' element={<CampsitesDirectoryPage />}/>
+          <Route path='directory/:campsiteId' element={<CampsiteDetailPage />} />
+      </Routes>
       <Footer />
     </div>
   );
